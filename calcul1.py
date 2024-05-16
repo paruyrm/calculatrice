b4e6d51cf16492cc89637cc9bac8a2faca4600fa
Paruyr <paruyrm@yhoo.com>
2024-05-16 15:08:11 +0200

    fonction addition

---
+++ calcul1.py
@@ -0,0 +1,6 @@
+num1 = float(input("Entrez le premier nombre : "))
+num2 = float(input("Entrez le deuxième nombre : "))
+
+# Addition des deux nombres
+sum = num1 + num2
+print(f"La somme de {num1} et {num2} est {sum}")
